a81f4ab682fead43e9ff11daa57db990b04efddf
rfargo <renaldy_fargo@yahoo.com>
2021-07-09 17:33:52 +0800

    Upload File

---
+++ be2/photo.js
@@ -0,0 +1,5 @@
+exports.index = function(req, res){
+  res.send('Hello World! Photo')
+
+  //res.render('index', { title: 'Route Separation Example' });
+};
---
+++ be2/public-access.js
@@ -0,0 +1,52 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'bumi_test'
+})
+
+connection.connect()
+
+exports.allAlbum = function (req, res) {
+  connection.query('SELECT * from photos', function (error, results, fields) {
+    if (error) throw error
+    res.send(results);
+  })
+}
+
+exports.filter = function (req, res) {
+  connection.query('SELECT * FROM `photos` WHERE `album_id` = ?', [req.params.album], 
+    function (error, results, fields) {
+      res.send(results);
+  });
+}
+
+exports.search = function (req, res) {
+  if(req.params.name){
+    connection.query('SELECT * FROM `photos` WHERE `title` LIKE ?', ['%' +req.params.name + '%'], 
+      function (error, results, fields) {
+        if (error) throw error
+        console.log(results);
+        res.send(results);
+    });    
+  }else{
+    res.send([]);
+  }
+
+}
+
+exports.albumList = function(req, res){
+  connection.query('SELECT DISTINCT `album_id` FROM `photos` ORDER BY `album_id`', 
+    function (error, results, fields) {
+      res.send(results);
+  });
+}
+
+exports.detail = function(req, res){
+  connection.query('SELECT * FROM `photos` WHERE `id` = ?', [req.params.id], 
+    function (error, results, fields) {
+      res.send(results);
+  });
+}
